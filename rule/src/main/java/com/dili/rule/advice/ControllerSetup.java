a81079dd0572e43cb8ef277f90c05a08d7ecd702
wangguofeng <wangguofeng@diligrp.com>
2020-09-15 15:41:20 +0800

    fix bug（去空格）-3

---
+++ rule/src/main/java/com/dili/rule/advice/ControllerSetup.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dili.rule.advice;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class ControllerSetup {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringtrimmer = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringtrimmer);
+    }
+}
--- rule/src/main/java/com/dili/rule/config/WebMvcConfig.java
+++ rule/src/main/java/com/dili/rule/config/WebMvcConfig.java
@@ -5,10 +5,6 @@
  */
 package com.dili.rule.config;
 
-import com.jxxc.admin.filters.ParameterTrimFilter;
-import javax.servlet.DispatcherType;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -19,14 +15,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebMvcConfig {
 
-    //@Bean(name = "parameterTrimFilter") // 不写name属性，默认beanName为方法名
-    public FilterRegistrationBean<ParameterTrimFilter> parameterTrimFilter() {
-        FilterRegistrationBean<ParameterTrimFilter> filter = new FilterRegistrationBean<>();
-        filter.setDispatcherTypes(DispatcherType.REQUEST);
-        filter.setFilter(new ParameterTrimFilter()); // 必须设置
-        filter.addUrlPatterns("/*"); // 拦截所有请求，如果没有设置则默认“/*”
-        filter.setName("parameterTrimFilter"); // 设置注册的名称，如果没有指定会使用Bean的名称。此name也是过滤器的名称
-        filter.setOrder(FilterRegistrationBean.LOWEST_PRECEDENCE);// 该filter在filterChain中的执行顺序
-        return filter;
-    }
+
 }
