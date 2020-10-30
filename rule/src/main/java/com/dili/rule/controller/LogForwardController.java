1cf536117110b5ba9d0bedc128259995f71b99aa
admin <admin@DESKTOP-7L9137R>
2020-10-30 15:03:59 +0800

    添加日志

--- rule/src/main/java/com/dili/rule/controller/LogForwardController.java
+++ rule/src/main/java/com/dili/rule/controller/LogForwardController.java
@@ -16,27 +16,43 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import cn.hutool.http.HttpRequest;
 import one.util.streamex.StreamEx;
 
+/**
+ * 日志转发
+ * 
+ * @author admin
+ *
+ */
 @Controller
 @RequestMapping("/logForwardController")
 public class LogForwardController {
 	@Value("${logger.contextPath:http://logger.diligrp.com:8283/api/businessLog/save}")
 	private String loggerContextPath;
 
-	private ObjectMapper  mapper=new ObjectMapper();
+	private ObjectMapper mapper = new ObjectMapper();
+
+	/**
+	 * 转发
+	 * 
+	 * @param req
+	 * @param logObj
+	 * @return
+	 * @throws JsonProcessingException
+	 */
 	@RequestMapping("/sendLog.action")
-	public Object sendLog(HttpServletRequest req,@RequestBody Map<String,Object>logObj) throws JsonProcessingException {
-		String remoteIp=req.getRemoteHost();
+	public Object sendLog(HttpServletRequest req, @RequestBody Map<String, Object> logObj)
+			throws JsonProcessingException {
+		String remoteIp = req.getRemoteHost();
 		logObj.put("remoteIp", remoteIp);
 		logObj.put("serverIp", req.getLocalAddr());
-		String requestBody=mapper.writeValueAsString(logObj);
-		Map<String,List<String>>headers=StreamEx.of(req.getHeaderNames()).mapToEntry(name->name,name->{
+		String requestBody = mapper.writeValueAsString(logObj);
+		Map<String, List<String>> headers = StreamEx.of(req.getHeaderNames()).mapToEntry(name -> name, name -> {
 			return req.getHeaders(name);
-		}).mapValues(enu->{
+		}).mapValues(enu -> {
 			return StreamEx.of(enu).toList();
-			
+
 		}).toMap();
-		
-		String respBody=HttpRequest.post(loggerContextPath).header(headers).body(requestBody).execute().body();
+
+		String respBody = HttpRequest.post(loggerContextPath).header(headers).body(requestBody).execute().body();
 		return mapper.readValue(respBody, Map.class);
 
 	}
