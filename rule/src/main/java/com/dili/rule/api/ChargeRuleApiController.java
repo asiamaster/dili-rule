3374ed68f809310c0c7eb9b9bd97594c8a69987f
wangguofeng <wangguofeng@diligr.com>
2021-01-18 16:56:26 +0800

    fix bug

--- rule/src/main/java/com/dili/rule/api/ChargeRuleApiController.java
+++ rule/src/main/java/com/dili/rule/api/ChargeRuleApiController.java
@@ -1,6 +1,7 @@
 package com.dili.rule.api;
 
 import cn.hutool.core.collection.CollectionUtil;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.dili.rule.sdk.domain.input.QueryFeeInput;
 import com.dili.rule.sdk.domain.output.QueryFeeOutput;
@@ -168,6 +169,7 @@ public class ChargeRuleApiController {
         List<QueryFeeOutput> resultList = new ArrayList<>();
         for (QueryFeeInput queryFeeInput : queryFeeInputList) {
             QueryFeeOutput output = this.queryRule(queryFeeInput);
+            logger.info("output={}", JSON.toJSONString(output));
             if (shortcut) {
                 if (StringUtils.isNotBlank(output.getMessage())) {
                     return BaseOutput.failure(output.getMessage()).setCode(output.getCode());
