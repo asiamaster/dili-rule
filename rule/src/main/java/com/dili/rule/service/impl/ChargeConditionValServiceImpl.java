48f42b564b47e4530b7eb00a729d80103ab2c816
wangguofeng <wangguofeng@diligr.com>
2021-05-14 15:11:45 +0800

    复制规则功能问题修复

--- rule/src/main/java/com/dili/rule/service/impl/ChargeConditionValServiceImpl.java
+++ rule/src/main/java/com/dili/rule/service/impl/ChargeConditionValServiceImpl.java
@@ -28,6 +28,8 @@ import com.dili.rule.service.remote.RemoteDataQueryService;
 import com.dili.ss.base.BaseServiceImpl;
 import com.google.common.collect.Lists;
 
+import one.util.streamex.StreamEx;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,9 +47,10 @@ import org.slf4j.LoggerFactory;
  */
 @Service
 public class ChargeConditionValServiceImpl extends BaseServiceImpl<ChargeConditionVal, Long> implements ChargeConditionValService {
-    private static final Logger logger=LoggerFactory.getLogger(ChargeConditionValServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(ChargeConditionValServiceImpl.class);
+
     public ChargeConditionValMapper getActualMapper() {
-        return (ChargeConditionValMapper)getDao();
+        return (ChargeConditionValMapper) getDao();
     }
 
     @Autowired
@@ -60,8 +63,8 @@ public class ChargeConditionValServiceImpl extends BaseServiceImpl<ChargeConditi
     private RemoteDataQueryService remoteDataQueryService;
 
     @Override
-    public Map<String, Object> getRuleCondition(ChargeRule chargeRule,String sessionId) {
-        Map<String,Object>resultMap = new HashMap<>();
+    public Map<String, Object> getRuleCondition(ChargeRule chargeRule, String sessionId) {
+        Map<String, Object> resultMap = new HashMap<>();
         List<ChargeConditionVal> chargeConditionValList = Lists.newArrayList();
         if (Objects.nonNull(chargeRule.getId())) {
             //根据规则，查询规则条件信息
@@ -69,28 +72,28 @@ public class ChargeConditionValServiceImpl extends BaseServiceImpl<ChargeConditi
             conditionVal.setRuleId(chargeRule.getId());
             chargeConditionValList = list(conditionVal);
         }
-        resultMap.put("chargeConditionVals",chargeConditionValList);
+        resultMap.put("chargeConditionVals", chargeConditionValList);
         //根据规则所在的市场、系统、业务，查询预定义的规则条件
         ConditionDefinition conditionDefinition = new ConditionDefinition();
         conditionDefinition.setMarketId(chargeRule.getMarketId());
         conditionDefinition.setBusinessType(chargeRule.getBusinessType());
         conditionDefinition.setRuleCondition(YesOrNoEnum.YES.getCode());
         conditionDefinition.setTargetType(TargetTypeEnum.CONDITION.getCode());
-               
+
         conditionDefinition.setSort("dataSourceId,matchType,id");
         conditionDefinition.setOrder("desc,asc,asc");
-        
+
         List<ConditionDefinition> conditionDefinitionList = conditionDefinitionService.listByExample(conditionDefinition);
         //组装已选条件与预定义的条件值
-        List<ConditionDefinitionVo> conditionDefinitions = generate(chargeConditionValList,conditionDefinitionList,sessionId);
-        resultMap.put("conditionDefinitions",conditionDefinitions);
+        List<ConditionDefinitionVo> conditionDefinitions = generate(chargeConditionValList, conditionDefinitionList, sessionId);
+        resultMap.put("conditionDefinitions", conditionDefinitions);
 
         return resultMap;
     }
 
     @Override
     public Integer deleteByRuleId(Long ruleId) {
-        if (Objects.isNull(ruleId)){
+        if (Objects.isNull(ruleId)) {
             return 0;
         }
         ChargeConditionVal val = new ChargeConditionVal();
@@ -103,10 +106,11 @@ public class ChargeConditionValServiceImpl extends BaseServiceImpl<ChargeConditi
 
     /**
      * 组装已选规则条件与预定义数据
-     * @param chargeConditionValList 已设置的规则条件
+     *
+     * @param chargeConditionValList  已设置的规则条件
      * @param conditionDefinitionList 已设置的预定义条件
      */
-    private List<ConditionDefinitionVo> generate(List<ChargeConditionVal> chargeConditionValList,List<ConditionDefinition> conditionDefinitionList,String sessionId){
+    private List<ConditionDefinitionVo> generate(List<ChargeConditionVal> chargeConditionValList, List<ConditionDefinition> conditionDefinitionList, String sessionId) {
         Map<Long, ChargeConditionVal> conditionValMap = chargeConditionValList.stream().collect(Collectors.toMap(ChargeConditionVal::getDefinitionId, RuleConditionVal -> RuleConditionVal));
         List<ConditionDefinitionVo> voList = Lists.newArrayList();
         conditionDefinitionList.stream().unordered().forEach(conditionDefinition -> {
@@ -123,41 +127,47 @@ public class ChargeConditionValServiceImpl extends BaseServiceImpl<ChargeConditi
                 } else if (matchType == MatchTypeEnum.IN) {
                     vo.getValues().addAll(objects);
                     DataSourceDefinition dataSourceDefinition = dataSourceDefinitionService.get(conditionDefinition.getDataSourceId());
-                    logger.info("objects={}",objects);
-                    if (Objects.nonNull(dataSourceDefinition)){
+                    logger.info("objects={}", objects);
+                    if (Objects.nonNull(dataSourceDefinition)) {
                         String matchColumn = conditionDefinition.getMatchColumn();
-                        List<Map<String, Object>> keyTextMap = remoteDataQueryService.queryKeys(dataSourceDefinition, objects,sessionId);
+                        List<Map<String, Object>> keyTextMap = remoteDataQueryService.queryKeys(dataSourceDefinition, objects, sessionId);
 //                        logger.info("keyTextMap={}",keyTextMap);
-                        if(keyTextMap!=null&&!keyTextMap.isEmpty()) {
+                        if (keyTextMap != null && !keyTextMap.isEmpty()) {
                             DataSourceColumn condition = new DataSourceColumn();
                             condition.setDataSourceId(conditionDefinition.getDataSourceId());
                             List<DataSourceColumn> columns = dataSourceColumnService.list(condition);
                             for (Object value : objects) {
+                                List<String> displayedText = new ArrayList<>();
                                 for (Map<String, Object> row : keyTextMap) {
                                     Object matchValue = row.get(matchColumn);
                                     if (String.valueOf(value).equals(String.valueOf(matchValue))) {
 //                                        logger.info("value={},matchColumn={}，matchValue={}",value,matchColumn,matchValue);
-                                        List<String> displayedText = new ArrayList<>();
+
                                         for (DataSourceColumn column : columns) {
                                             if (YesOrNoEnum.YES.getCode().equals(column.getDisplay())) {
-                                                Object obj=row.get(column.getColumnCode());
+                                                Object obj = row.get(column.getColumnCode());
 //                                                logger.info("displayedText={}",obj);
-                                                if(obj!=null) {
+                                                if (obj != null) {
                                                     displayedText.add(String.valueOf(obj));
                                                 }
 
                                             }
                                         }
-                                        vo.getTexts().add(String.join("#", displayedText));
+
                                     }
                                 }
+                                vo.getTexts().add(String.join("#", displayedText));
                             }
-                        }else {
-                        	vo.getTexts().add(objects);
+                        } else {
+                            vo.getTexts().addAll(StreamEx.of(objects).map(o -> {
+                                return o == null ? "" : String.valueOf(o);
+                            }).map(StringUtils::trimToEmpty).toList());
                         }
 
-                    }else{
-                        vo.getTexts().add(objects);
+                    } else {
+                        vo.getTexts().addAll(StreamEx.of(objects).map(o -> {
+                            return o == null ? "" : String.valueOf(o);
+                        }).map(StringUtils::trimToEmpty).toList());
                     }
 
                 }
@@ -167,23 +177,24 @@ public class ChargeConditionValServiceImpl extends BaseServiceImpl<ChargeConditi
         return voList;
     }
 
-	@Override
-	public List<ConditionDefinition> getRuleVariable(ChargeRule chargeRule,Optional<ValueDataTypeEnum> dataType) {
+    @Override
+    public List<ConditionDefinition> getRuleVariable(ChargeRule chargeRule, Optional<ValueDataTypeEnum> dataType) {
         Map<String, Object> resultMap = new HashMap<>();
         //根据规则所在的市场、系统、业务，查询预定义的规则条件
         ConditionDefinition conditionDefinition = new ConditionDefinition();
         conditionDefinition.setMarketId(chargeRule.getMarketId());
         conditionDefinition.setBusinessType(chargeRule.getBusinessType());
         conditionDefinition.setTargetType(TargetTypeEnum.VARIABLE.getCode());
-       
+
         conditionDefinition.setSort("matchType,id");
         conditionDefinition.setOrder("asc,asc");
-        
-        dataType.ifPresent(dt->{
+
+        dataType.ifPresent(dt -> {
             conditionDefinition.setDataType(dt.getCode());
         });
 
         List<ConditionDefinition> conditionDefinitionList = conditionDefinitionService.list(conditionDefinition);
         return conditionDefinitionList;
-	}
+    }
+
 }
